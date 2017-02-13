39562f36861d1897bf070b829f361ad177d6d8c3
Ilya Shusterman <ilyashusterman@outlook.com>
2017-02-13 15:35:42 +0200

    initial commit

--- Thermos.py
+++ Thermos.py
@@ -1,8 +1,20 @@
 from mimetypes import init
-
-from flask import Flask, render_template, url_for
+from logging import DEBUG
+from datetime import datetime
+from flask import Flask, render_template, request, flash, redirect, url_for
 
 app = Flask(__name__)
+app.logger.setLevel(DEBUG)
+
+images = []
+
+
+def store_image(image):
+    images.append(dict(
+        image=image,
+        user="ilya",
+        date=datetime.utcnow()
+    ))
 
 
 class User:
@@ -26,10 +38,27 @@ def index():
                            text="Text passed from view to template", user=User("ilya", "shusterman"))
 
 
-@app.route('/add')
+@app.route('/add', methods=['GET', 'POST'])
 def add():
+    if request.method == "POST":
+        image = request.form['image']
+        file_image = request.files
+        store_image(image)
+        store_image(file_image)
+        app.logger.debug('stored images : ' + image)
+        return redirect(url_for('index'))
     return render_template('add.html')
 
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def server_error(e):
+    return render_template('500.html'), 500
+
+
 if __name__ == '__main__':
-    app.run()  # to get app running in debug is debug=True
+    app.run(debug=True)  # to get app running in debug is debug=True
